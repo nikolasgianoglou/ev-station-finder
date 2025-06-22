2c1046b366f1255d5e05cad75fbd53b2348d98b9
nikolasgianoglou <nikolasgcoelho@gmail.com>
2025-06-22 20:38:17 -0300

    Creation of the initial project

---
+++ EV Station Finder/EV Station Finder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EV Station Finder
+//
+//  Created by Nikolas Gianoglou on 22/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EV Station Finder/EV Station Finder/EV_Station_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EV_Station_FinderApp.swift
+//  EV Station Finder
+//
+//  Created by Nikolas Gianoglou on 22/06/25.
+//
+
+import SwiftUI
+
+@main
+struct EV_Station_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
